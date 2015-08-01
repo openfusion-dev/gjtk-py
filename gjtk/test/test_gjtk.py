503645f291541ef7ef63b185a62d9f02a4a9aa9b
dtucker <david.tucker@isilon.com>
2015-08-01 00:21:54 -0700

    Add a test skeleton.

---
+++ gjtk/test/test_gjtk.py
@@ -0,0 +1,18 @@
+
+
+import unittest
+
+import gjtk
+
+
+class GJTKTest (unittest.TestCase):
+
+
+    def test_gjtk(self):
+        raise NotImplementedError()
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
