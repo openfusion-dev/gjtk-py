af9b24379acbbb610c71efee7eae8690a14f8e15
dtucker <david.tucker@isilon.com>
2015-08-06 23:48:10 -0700

    Break random tests into TestCases.

--- gjtk/test/test_random.py
+++ gjtk/test/test_random.py
@@ -5,9 +5,9 @@ import unittest
 import gjtk
 
 
-class RandomTest (unittest.TestCase):
+class PositionTest (unittest.TestCase):
 
-    def test_randomPosition(self):
+    def test_valid_Position(self):
         """ should return a valid Position """
         test_data = gjtk.random.Position(max_numbers=6)
         self.assertTrue(
@@ -15,7 +15,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomPointCoordinates(self):
+
+class PointCoordinatesTest (unittest.TestCase):
+
+    def test_valid_PointCoordinates(self):
         """ should return a valid Point coordinates """
         test_data = gjtk.random.PointCoordinates()
         self.assertTrue(
@@ -23,7 +26,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomMultiPointCoordinates(self):
+
+class MultiPointCoordinatesTest (unittest.TestCase):
+
+    def test_valid_MultiPointCoordinates(self):
         """ should return a valid MultiPoint coordinates """
         test_data = gjtk.random.MultiPointCoordinates()
         self.assertTrue(
@@ -31,7 +37,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomLineStringCoordinates(self):
+
+class LineStringCoordinatesTest (unittest.TestCase):
+
+    def test_valid_LineStringCoordinates(self):
         """ should return a valid LineString coordinates """
         test_data = gjtk.random.LineStringCoordinates()
         self.assertTrue(
@@ -39,7 +48,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomLinearRingCoordinates(self):
+
+class LinearRingCoordinatesTest (unittest.TestCase):
+
+    def test_valid_LinearRingCoordinates(self):
         """ should return a valid LinearRing coordinates """
         test_data = gjtk.random.LinearRingCoordinates()
         self.assertTrue(
@@ -47,7 +59,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomMultiLineStringCoordinates(self):
+
+class MultiLineStringCoordinatesTest (unittest.TestCase):
+
+    def test_valid_MultiLineStringCoordinates(self):
         """ should return a valid MultiLineString coordinates """
         test_data = gjtk.random.MultiLineStringCoordinates()
         self.assertTrue(
@@ -55,7 +70,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomPolygonCoordinates(self):
+
+class PolygonCoordinatesTest (unittest.TestCase):
+
+    def test_valid_PolygonCoordinates(self):
         """ should return a valid Polygon coordinates """
         test_data = gjtk.random.PolygonCoordinates()
         self.assertTrue(
@@ -63,7 +81,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomMultiPolygonCoordinates(self):
+
+class MultiPolygonCoordinatesTest (unittest.TestCase):
+
+    def test_valid_MultiPolygonCoordinates(self):
         """ should return a valid MultiPolygon coordinates """
         test_data = gjtk.random.MultiPolygonCoordinates()
         self.assertTrue(
@@ -71,7 +92,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomGeometry(self):
+
+class GeometryTest (unittest.TestCase):
+
+    def test_valid_Geometry(self):
         """ should return a valid Geometry """
         test_data = gjtk.random.Geometry()
         self.assertTrue(
@@ -79,7 +103,21 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomMultiPoint(self):
+
+class PointTest (unittest.TestCase):
+
+    def test_valid_Point(self):
+        """ should return a valid Point """
+        test_data = gjtk.random.Point()
+        self.assertTrue(
+            gjtk.validate.isPoint(test_data),
+            gjtk.test.error_message(test_data)
+        )
+
+
+class MultiPointTest (unittest.TestCase):
+
+    def test_valid_MultiPoint(self):
         """ should return a valid MultiPoint """
         test_data = gjtk.random.MultiPoint()
         self.assertTrue(
@@ -87,7 +125,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomLineString(self):
+
+class LineStringTest (unittest.TestCase):
+
+    def test_valid_LineString(self):
         """ should return a valid LineString """
         test_data = gjtk.random.LineString()
         self.assertTrue(
@@ -95,7 +136,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomMultiLineString(self):
+
+class MultiLineStringTest (unittest.TestCase):
+
+    def test_valid_MultiLineString(self):
         """ should return a valid MultiLineString """
         test_data = gjtk.random.MultiLineString()
         self.assertTrue(
@@ -103,7 +147,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomPolygon(self):
+
+class PolygonTest (unittest.TestCase):
+
+    def test_valid_Polygon(self):
         """ should return a valid Polygon """
         test_data = gjtk.random.Polygon()
         self.assertTrue(
@@ -111,7 +158,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomMultiPolygon(self):
+
+class MultiPolygonTest (unittest.TestCase):
+
+    def test_valid_MultiPolygon(self):
         """ should return a valid MultiPolygon """
         test_data = gjtk.random.MultiPolygon()
         self.assertTrue(
@@ -119,7 +169,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomGeometryCollection(self):
+
+class GeometryCollectionTest (unittest.TestCase):
+
+    def test_valid_GeometryCollection(self):
         """ should return a valid GeometryCollection """
         test_data = gjtk.random.GeometryCollection()
         self.assertTrue(
@@ -127,15 +180,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomGeometryCollection(self):
-        """ should return a valid GeometryCollection """
-        test_data = gjtk.random.GeometryCollection()
-        self.assertTrue(
-            gjtk.validate.isGeometryCollection(test_data),
-            gjtk.test.error_message(test_data)
-        )
 
-    def test_randomFeature(self):
+class FeatureTest (unittest.TestCase):
+
+    def test_valid_Feature(self):
         """ should return a valid Feature """
         test_data = gjtk.random.Feature()
         self.assertTrue(
@@ -143,7 +191,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomFeatureCollection(self):
+
+class FeatureCollectionTest (unittest.TestCase):
+
+    def test_valid_FeatureCollection(self):
         """ should return a valid FeatureCollection """
         test_data = gjtk.random.FeatureCollection()
         self.assertTrue(
@@ -151,7 +202,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomCRS(self):
+
+class CRSTest (unittest.TestCase):
+
+    def test_valid_CRS(self):
         """ should return a valid CRS """
         test_data = gjtk.random.CRS()
         self.assertTrue(
@@ -159,7 +213,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomLink(self):
+
+class LinkTest (unittest.TestCase):
+
+    def test_valid_Link(self):
         """ should return a valid Link """
         test_data = gjtk.random.Link()
         self.assertTrue(
@@ -167,7 +224,10 @@ class RandomTest (unittest.TestCase):
             gjtk.test.error_message(test_data)
         )
 
-    def test_randomBbox(self):
+
+class BboxTest (unittest.TestCase):
+
+    def test_valid_Bbox(self):
         """ should return a valid Bbox """
         test_data = gjtk.random.Bbox()
         self.assertTrue(
