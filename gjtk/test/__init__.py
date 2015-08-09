20e9d6b995576fdc4dcec7c62412b09ac34f5f5a
dtucker <david.michael.tucker@gmail.com>
2015-08-09 16:33:34 -0700

    Do not crash in gjtk.test.error_message when printing non-JSON.

--- gjtk/test/__init__.py
+++ gjtk/test/__init__.py
@@ -7,23 +7,29 @@ def error_message(test_data=None, test_result=None):
     message = ['']
     if test_data is not None:
         message.append('// Data')
-        message.append(
-            json.dumps(
-                test_data,
-                sort_keys=True,
-                indent=2,
-                separators=(',', ': ')
+        try:
+            message.append(
+                json.dumps(
+                    test_data,
+                    sort_keys=True,
+                    indent=2,
+                    separators=(',', ': ')
+                )
             )
-        )
+        except:
+            message.append('The test data is not JSON serializable:\n{0}'.format(test_data))
     if test_result is not None:
         message.append('// Result')
-        message.append(
-            json.dumps(
-                test_result,
-                sort_keys=True,
-                indent=2,
-                separators=(',', ': ')
+        try:
+            message.append(
+                json.dumps(
+                    test_result,
+                    sort_keys=True,
+                    indent=2,
+                    separators=(',', ': ')
+                )
             )
-        )
+        except:
+            message.append('The test result is not JSON serializable:\n{0}'.format(test_result))
     return '\n'.join(message)
 
