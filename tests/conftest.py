299279c24f1d4dd8817ff74b24f9208f3c56b105
David Tucker <david@tucker.name>
2020-01-26 23:24:54 -0800

    Move gjtk.test to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@
 from __future__ import absolute_import
 
 # pylint: disable=wildcard-import, unused-wildcard-import
-from gjtk.test.fixtures import *
+from .fixtures import *
 from gjtk.random import *
